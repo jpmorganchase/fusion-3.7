1401972d9834b14856dce6f08b572ccea9315e82
Kalpesh Palav <kalpesh.palav@jpmchase.com>
2025-12-09 16:11:06 +0000

    Release 20251209

--- src/fusion/__init__.py
+++ src/fusion/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = "Fusion Devs"
 __email__ = "fusion_developers@jpmorgan.com"
-__version__ = "2.0.0-dev0"
+__version__ = "2.0.0"
 
 from .fusion import Fusion  # Import the core Fusion class
 
